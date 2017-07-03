010ba56d23b1bba61215109bba846a7d14aa32b2
toshimaki <toshimaki@users.noreply.github.com>
2017-07-03 17:18:22 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,19 @@
+var separate_time = function(time){
+var sec   = time.getSeconds();
+var min   = time.getMinutes();
+var hours = time.getHours();
+var days  = time.getDays();
+var month = time.getManth();
+var year  = time.getFullYear();
+return[sec, min, hours, days, month, years];
+}
+
+var now = new Date();
+var counter = separate_time(now);
+document.getElementById('countdown').textContent =
+counter[5]+ '年' +
+counter[4]+ '月' +
+counter[3]+ '日' +
+counter[2]+ '時' +
+counter[1]+ '分' +
+counter[0]+ '秒';
