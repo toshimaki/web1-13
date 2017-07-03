22842973d0670dedc2aa6fc236bbd12cf17c5768
toshimaki <toshimaki@users.noreply.github.com>
2017-07-03 18:16:25 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,20 @@
+function getFileName() {
+  return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
+}else{
+  opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
+
+document.getElementById('form').select.onchange = function(){
+  location.href = document.getElementById('form')>select.value;
+}
+
 var separate_time = function(time){
 var sec   = time.getSeconds();
 var min   = time.getMinutes();
@@ -12,9 +29,7 @@ var update = function(){
 var now = new Date();
 var counter = separate_time(now);
   document.getElementById('countdown').textContent=
-document.getElementById('form').select.onchange = function(){
-  location.href = document.getElementById('form').select.value;
-}
+
 counter[5]+ '年' +
 counter[4]+ '月' +
 counter[3]+ '日' +
