305646c82666d6f16c04c52e3d375a2b2fb89e19
toshimaki <toshimaki@users.noreply.github.com>
2017-07-03 17:40:53 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -5,7 +5,7 @@ var hours = time.getHours();
 var days  = time.getDate();
 var month = time.getMonth();
 var year  = time.getFullYear();
-return[sec, min, hours, days, month, years];
+return[sec, min, hours, days, month, year];
 }
 
 var now = new Date();
